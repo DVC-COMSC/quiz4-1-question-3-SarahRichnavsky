c4dea8d386bea4ddf19fe3cc13c108f2cec98d17
SarahRichnavsky <112355091+SarahRichnavsky@users.noreply.github.com>
2022-10-12 22:34:12 +0000

    hello

--- main.cpp
+++ main.cpp
@@ -1,9 +1,44 @@
-// Complete the following program. 
 #include <iostream>
 using namespace std;
 
 int main()
 {
 	int num1, num2, num3;
-	
-}
+	int min, max, other;
+
+	cin >> num1 >> num2 >> num3;
+
+	if (num1 > num2)
+	{
+		if ( num2 < num3)
+			min = num2; 
+		else // num2 >= num3
+			min =   num3;
+	}
+	else // num1 <= num2
+	{
+		if (num1 < num3)
+			min = num1;
+		else // num >= num1
+			min = num3;
+	}
+	//To find max
+	if (num1 > num2)
+	{
+		if ( num1 >  num3)
+			max = num1;
+		else // num1 <= num3
+			max = num3;
+	}
+	else // num1 <= num2
+	{
+		if (num2 < num3)
+			max = num3;
+		else // num >= num1
+			max = num2;
+	}
+		
+	cout << "max : " << max << endl;
+	cout << "min: "  << min << endl; 
+		
+} 
